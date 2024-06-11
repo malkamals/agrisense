62d23685d71eac14ec4b245a6455b0f6892a7eb5
Ragraze <83755597+Ragraze@users.noreply.github.com>
2024-06-11 09:20:32 +0700

    Update index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,6 @@
 const express = require("express");
 const router = express.Router();
 const {
-	verifyTokenBeforeLogin,
-	verifyTokenAfterLogin,
-} = require("../middleware");
-
-const {
 	registerUser,
 	loginUser,
 	logoutUser,
